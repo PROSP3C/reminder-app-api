806bfd042b54763c54c93010181aaa9eeabe6a87
Declan Possnett <declanpossnett@gmail.com>
2025-12-22 20:29:37 +0000

    update tsconfig

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,7 @@
 import { Pool } from 'pg'
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === 'production'
-      ? process.env.DATABASE_URL
-      : (import.meta as any).env.VITE_DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 })
 
