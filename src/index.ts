6ee55f3e9345a5f3a2f5c9a349aac5f089d0f8dd
Declan Possnett <declanpossnett@gmail.com>
2025-12-22 19:39:38 +0000

    debug allow all

--- src/index.ts
+++ src/index.ts
@@ -16,19 +16,21 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(
-  cors({
-    origin: [
-      'http://localhost:9000',
-      'capacitor://localhost',
-      'http://localhost',
-      'https://your-railway-url.up.railway.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  }),
-)
+// app.use(
+//   cors({
+//     origin: [
+//       'http://localhost:9000',
+//       'capacitor://localhost',
+//       'http://localhost',
+//       'https://your-railway-url.up.railway.app',
+//     ],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//     credentials: true,
+//   }),
+// )
+
+cors({ origin: '*' })
 
 app.use('/auth', authRouter)
 app.use('/reminders', remindersRouter)
