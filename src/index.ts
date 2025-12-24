94774c665023d602630a6dc0ea6dabd5c5e50231
Declan Possnett <declanpossnett@gmail.com>
2025-12-24 09:29:14 +0000

    allow capacitor cors

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ app.use(
       'http://localhost',
       'http://localhost:9000',
       'https://reminder-app-api-production.up.railway.app',
+      'http://192.168.1.99:9500',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
